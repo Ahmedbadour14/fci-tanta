ba2dd4286baa381503199d6ccf023906d8964e49
Ahmed Badour <ahmed.badour2005@gmail.com>
2026-05-14 01:21:02 +0300

    update dean image path

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -121,7 +121,7 @@ const About = () => {
           <div className="flex flex-col md:flex-row">
             <div className="w-full md:w-2/5 relative h-64 md:h-auto">
               <img
-                src="/images/197933259.png"
+                src="/images/dean.jpg"
                 alt={t('home.deanName')}
                 className="absolute inset-0 w-full h-full object-cover"
               />
